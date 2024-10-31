cfe3ddd140e2381effe6fe73f6cb0a6341d0b328
NikitaOnianov <nikita.onianov@yandex.ru>
2024-10-31 11:51:52 +0300

    first commit

---
+++ recursive_sorting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace recursive_sorting
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите элементы массива: ");
+            var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var numbersArray = new int[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                numbersArray[i] = Convert.ToInt32(parts[i]);
+            }
+
+             
+            numbersArray = QuickSort(numbersArray, 0, numbersArray.Length - 1);
+            Console.WriteLine("Отсортированный массив: " + string.Join(" ", numbersArray));
+            Console.ReadKey();
+        }
+
+        static int[] QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(array, left, right);
+                QuickSort(array, left, pivot - 1);
+                QuickSort(array, pivot + 1, right);
+            }
+            return array;
+        }
+
+        static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+            int tamp = 0;
+            for (int j = left; j <= right - 1; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    tamp = array[i];
+                    array[i] = array[j];
+                    array[j] = tamp;
+                }
+            }
+            tamp = array[i + 1];
+            array[i + 1] = array[right];
+            array[right] = tamp;
+            return i + 1;
+        }
+    }
+}
---
+++ recursive_sorting/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("recursive_sorting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("recursive_sorting")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9e8ef0c0-9135-4a52-ae2d-a72c6dc965e9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
